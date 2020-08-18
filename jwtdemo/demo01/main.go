b56cc8766d9721babaccb335a374e14f15f96d89
sam <sam@163.com>
2020-08-18 14:06:10 +0800

    jwt test

--- jwtdemo/demo01/main.go
+++ jwtdemo/demo01/main.go
@@ -1,22 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
+	"time"
 )
 
 //构建和签名令牌的简单示例
 func main() {
 
+	//1.自定义一个secret
 	hmacSampleSecret:="qTw32gOomGLMAatO2HcmGp2hsq8P3cb7"
 
-	//创建一个新的令牌对象，指定签名方法和您希望包含的声明。
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"foo": "bar",
-		"name":"sam",
-	})
+	//2.声明想传递的一些声明
+	user:= map[string]interface{}{}
+	user["name"] = "sam"
+	user["age"] = 18
+	user["sex"] ="male"
+	dataByte,_:= json.Marshal(user)
+	var dataStr = string(dataByte)
 
-	//生成token串
+	//3.使用Claim保存json 这里是个例子，并包含了一个故意签发一个已过期的token
+	//data := jwt.StandardClaims{
+	//	 Subject:dataStr,
+	//	 ExpiresAt:time.Now().Unix()-1000}
+	//todo 您也可以使用非标准Claims，自己直接使用MapClaims也可以的额
+
+	data:= jwt.MapClaims{
+		"sub":dataStr,
+		"exp":time.Now().Unix() + 600,
+		"name":"sam",
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,data)
+
+	//4.生成token串
 	tokenString, err := token.SignedString([]byte(hmacSampleSecret))
 
 	fmt.Println(tokenString, err)
--- jwtdemo/demo02/main.go
+++ jwtdemo/demo02/main.go
@@ -6,32 +6,37 @@ import (
 )
 
 //解析和验证令牌的简单示例
+//@link https://www.jsonwebtoken.io/
 func main() {
 
 
 	//token字符串
-	tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIiLCJuYW1lIjoic2FtIiwibmJmIjoxNDQ0NDc4NDAwfQ.r9akkIdQg6eRdqZ-alF1piHn8yPfBbNGrHSNwJaDIjw"
+	tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1OTc3MzA5MjksIm5hbWUiOiJzYW0iLCJzdWIiOiJ7XCJhZ2VcIjoxOCxcIm5hbWVcIjpcInNhbVwiLFwic2V4XCI6XCJtYWxlXCJ9In0.iPZ2KJqyPPi2vObpVSxbq2CiUacY_4EFzxmmmEHIGqc"
 	hmacSampleSecret:=[]byte("qTw32gOomGLMAatO2HcmGp2hsq8P3cb7")
 
-    //解析
+	///将token字符串转换为token结构体
+	//todo 注意如果token的生成设置过期时间之后，则这里会主动检查token的过期情况的额
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		// Don't forget to validate the alg is what you expect:
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		}
 
-		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		//if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		//	return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		//}
 		return hmacSampleSecret, nil
 	})
+	fmt.Println(err) //如果过期的话，这里已经打出来了
 
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		fmt.Println(claims["foo"], claims["name"])
+		//todo 这里没有必要去主动验证是否过期了
+		////校验下token是否过期
+		//succ := claims.VerifyExpiresAt(time.Now().Unix(),true)
+		//fmt.Println("succ",succ)
+		//获取token中保存的用户信息
+        //fmt.Println(claims["sub"])
+		fmt.Printf("%#v\r\n",claims)
 	} else {
 		fmt.Println(err)
 	}
 
 
 
-
-	
 }
