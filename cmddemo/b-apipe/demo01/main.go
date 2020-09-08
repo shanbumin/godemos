09e7faaaf6cefda6eedbbed157ee049f77abcd24
sam <sam@163.com>
2020-09-08 14:08:57 +0800

    cmd

--- cmddemo/b-apipe/demo01/main.go
+++ cmddemo/b-apipe/demo01/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 
 
-	cmd0 := exec.Command("/bin/bash", "-c", "sleep 5")
+	cmd0 := exec.Command("/bin/bash", "-c", "sleep 5;echo shit")
 
 	//Start开始执行cmd0包含的命令，但并不会等待该命令完成即返回。Wait方法会返回命令的返回状态码并在命令返回后释放相关的资源。
 	if err := cmd0.Start(); err != nil {
@@ -16,7 +16,7 @@ func main() {
 		return
 	}
 
-
+     //todo 可以打开注释进行相关调试
 	//当使用了Start执行命令之后，Wait方法可以确保它执行完毕，二者组合与Run的效果等价额
 	//if err := cmd0.Wait(); err != nil {
 	//	fmt.Printf("Error: Couldn't wait for the first command: %s\n", err)
