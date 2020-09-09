a3c93acfc7b2b985469d4cb317fc6bcba0ee9357
sam <sam@163.com>
2020-09-09 10:52:40 +0800

    signal

--- signaldemo/demo01/main.go
+++ signaldemo/demo01/main.go
@@ -6,8 +6,17 @@ import (
 	"os/signal"
 )
 
+
+//todo 在调试的时候，当执行go run main.go的时候，千万别通过ps aux|grep main.go来获取当前进程的id
+//todo go run main.go本质是会发起一个子进程自动编译成二进制文件执行的，所以执行的程序应该是那个二进制才对
+//todo 可以通过这种方式查看: ps aux|grep "go-build"  或者 ps aux|grep "/var/folders"
+//todo 或者我们直接在程序中打印出也可以的额  fmt.Println(os.Getpid())
+
 // 监听全部信号
 func main()  {
+
+	fmt.Println(os.Getpid())
+	 
 	//创建chan
 	c := make(chan os.Signal)
 	//监听所有信号
@@ -19,9 +28,8 @@ func main()  {
 }
 
 //go run main.go
-//启动
-//ctrl+c退出,输出
-//退出信号 interrupt
-//*************************
-//kill pid 输出
-//退出信号 Terminated: 15
+//1.ctrl+c退出,输出
+// kill pid
+// kill -30 pid
+// kill -31 pid
+// kill -9  pid #执行默认操作
--- signaldemo/demo04/main.go
+++ signaldemo/demo04/main.go
@@ -12,11 +12,12 @@ import (
 
 
 //todo golang中多goroutine时kill信号接收的问题(一)
-//todo 多goroutine情况下，为什么kill就失灵了呢?
+//@link  https://blog.csdn.net/zhuxinquan61/article/details/89421634
 
 
 // 生产者: 生成 factor 整数倍的序列
 func Producer(factor int, out chan<- int) {
+	fmt.Println("p-",os.Getpid())
 	for i := 0; ; i++ {
 		out <- i*factor
 	}
@@ -24,6 +25,7 @@ func Producer(factor int, out chan<- int) {
 
 // 消费者
 func Consumer(in <-chan int) {
+	fmt.Println("c-",os.Getpid())
 	for v := range in {
 		time.Sleep(1 * time.Second)
 		fmt.Println(v)
@@ -35,14 +37,16 @@ func Consumer(in <-chan int) {
 //我们可以让`main`函数保存阻塞状态不退出，只有当用户输入`Ctrl-C`时才真正退出程序：
 
 func main() {
-   //ch := make(chan int, 64) // 成果队列
-   //go Producer(3, ch) // 生成 3 的倍数的序列
-   //go Producer(5, ch) // 生成 5 的倍数的序列
-   //go Consumer(ch)    // 消费 生成的队列
+   ch := make(chan int, 64) // 成果队列
+   go Producer(3, ch) // 生成 3 的倍数的序列
+   go Producer(5, ch) // 生成 5 的倍数的序列
+   go Consumer(ch)    // 消费 生成的队列
 
+	fmt.Println(os.Getpid())
    // Ctrl+C 退出
    sig := make(chan os.Signal,1)
-   signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM,os.Kill,syscall.SIGUSR1, syscall.SIGUSR2)
+   signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM,syscall.SIGUSR1, syscall.SIGUSR2)
    fmt.Printf("退出: (%v)\n", <-sig)
+   os.Exit(400)
 }
 
